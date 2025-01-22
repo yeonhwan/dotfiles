6c3dd0685acf238e632b9f835e1fe67375533a42
yeonhwan <yeonhwan619@gmail.com>
2025-01-22 16:53:52 +0900

    Change nvim config to replace telescope to fzf

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   -- telescope
   {
     "nvim-telescope/telescope.nvim",
+    enabled = false,
     keys = {
       { "<leader>fq", "<cmd>Telescope file_browser<cr>", desc = "File browser" },
       { "<leader>fi", "<cmd>Telescope import<cr>", desc = "Imports" },
@@ -11,11 +12,13 @@ return {
   -- telescope file browser
   {
     "nvim-telescope/telescope-file-browser.nvim",
+    enabled = false,
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   },
   -- telescope imports
   {
     "piersolenski/telescope-import.nvim",
+    enabled = false,
     dependencies = "nvim-telescope/telescope.nvim",
     config = function()
       require("telescope").load_extension("import")
