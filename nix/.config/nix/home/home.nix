f6dcdda3a6db5d6ff6af6733b6e735ae049b830f
yeonhwan <yeonhwan619@gmail.com>
2025-01-24 16:45:50 +0900

    Edit shell prompt setup and delete p10k

--- nix/.config/nix/home/home.nix
+++ nix/.config/nix/home/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:  # Remove other arguments for now
+{ pkgs,lib, ... }:  # Remove other arguments for now
 
 {
   home = {
@@ -27,6 +27,142 @@
       enableZshIntegration = true;
     };
 
+
+    # starship shell prompt
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+
+      settings = {
+        palettes = {
+          teal = {
+            tealwhite = "#e8fbfb";
+            teal100 = "#b2d8d8";
+            teal200 = "#66b2b2";
+            teal300 = "#008080";
+            teal400 = "#006666";
+            teal500 = "#004c4c";
+            tealsubtle = "#bec2c2";
+            tealblack = "#002727";
+
+          };
+        };
+        palette = "teal";
+
+        right_format = lib.concatStrings[
+          "$memory_usage"
+          "$localip"
+        ];
+
+        format = lib.concatStrings [ 
+          "[](teal100)"
+          "[  ](bg:teal100 fg:teal500)"
+          "$username"
+          "[](bg:teal200 fg:teal100)"
+          "$directory"
+          "[](fg:teal200 bg:teal300)"
+          "$git_branch"
+          "$git_status"
+          "[](fg:teal300 bg:teal400)"
+          "$nodejs"
+          "$rust"
+          "$golang"
+          "$php"
+          "$nix_shell"
+          "$lua"
+          "$python"
+          "[](fg:teal400 bg:teal500)"
+          "$time"
+          "[ ](fg:teal500)"
+          "$character" 
+        ];
+        username = { 
+          show_always = true;
+          style_user = "bold bg:teal100 fg:teal500";
+          style_root = "bg:teal100 fg:teal500";
+          format = "[ $user ]($style)";
+        };
+        continuation_prompt = "🏃";
+        directory = { 
+          style = "fg:teal400 bg:teal200";
+          format = "[ $path ]($style)";
+          truncation_length = 3;
+          truncation_symbol = "…/"; 
+
+          substitutions = {
+            "Documents" = "󰈙 ";
+            "Downloads" = " ";
+            "Music" = " ";
+            "Pictures" = " ";
+          };
+        };
+        localip = {
+          disabled = false;
+          ssh_only = false;
+          format = "🌐 [$localipv4](dimmed fg:tealwhite)"; 
+        };
+        memory_usage = {
+          disabled = false;
+          symbol = "🤖";
+          format = "$symbol [$ram_pct](dimmed fg:tealwhite) ";
+        };
+        git_branch = {
+          symbol = "";
+          style = "bg:teal300";
+          format = "[[ $symbol $branch ](fg:tealblack bg:teal300)]($style)";
+        };
+        git_status = {
+          style = "bg:teal300";
+          format = "[[($all_status$ahead_behind )](fg:tealblack bg:teal300)]($style)";
+        };
+        
+        # lang specific
+        nodejs = {
+          symbol = "";
+          style = "bg:teal400";
+          format = "[[ $symbol ($version) ](fg:tealwhite bg:teal400)]($style)";
+        };
+        rust = { 
+          symbol = "";
+          style = "bg:teal400";
+          format = "[[ $symbol ($version) ](fg:tealwhite bg:teal400)]($style)";
+        };
+
+        golang = { 
+          symbol = "";
+          style = "bg:teal400";
+          format = "[[ $symbol ($version) ](fg:tealwhite bg:teal400)]($style)";
+        };
+
+        php = { 
+          symbol = "";
+          style = "bg:teal400";
+          format = "[[ $symbol ($version) ](fg:tealwhite bg:teal400)]($style)" ;
+        };
+
+        nix_shell = { 
+          disabled = false;
+          symbol = "";
+          style = "bg:teal400";
+          format = "[[ $symbol ($version) ](fg:tealwhite bg:teal400)]($style)" ;
+        };
+
+        lua = { 
+          symbol = "";
+          style = "bg:teal400";
+          format = "[[ $symbol ($version) ](fg:tealwhite bg:teal400)]($style)" ;
+        };
+
+        time = { 
+          disabled = false;
+          # Hour:Minute Format
+          time_format = "%R";   
+          style = "bg:teal500";
+          format = "[[  $time ](fg:tealwhite bg:teal500)]($style)";
+        };
+      };
+    };
+
     # zsh configuration is done through nix-home-manager
     # .zshrc / .zshenv is locked
     zsh = {
@@ -42,12 +178,6 @@
         plugins = ["git"];
       };
 
-      initExtraFirst = ''
-        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-            fi
-            '';
-
       sessionVariables = {
         PATH = "/opt/homebrew/bin:/opt/homebrew/sbin:$HOME/.bun/bin:$HOME/.console-ninja/.bin:$PATH";
       };
@@ -61,6 +191,8 @@
       vim = "nvim";
       imgpr = "kitten icat $(fd -e jpg -e svg -e webp -e png -e jpeg -e gif | fzf)";
       buildflake = "darwin-rebuild switch --impure --flake \"$(readlink -f ~/.config/nix)\"#default";
+      # garbage collect twice as sudo & user
+      nixgc = "nix-collect-garbage -d && nix-collect-garbage -d";
       ls = "eza --icons=always";
       cd = "z";
       lg = "lazygit";
@@ -68,11 +200,6 @@
     };
 
     plugins = [
-      {
-        name = "zsh-powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
       {
         name = "zsh-autosuggestions";
         src = pkgs.zsh-autosuggestions;
@@ -89,10 +216,7 @@
     initExtra = ''
       # brew setup
       eval "$(/opt/homebrew/bin/brew shellenv)"
-      # P10k setup
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-      typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
-
+      
       # History setup
       setopt share_history
         setopt hist_expire_dups_first
