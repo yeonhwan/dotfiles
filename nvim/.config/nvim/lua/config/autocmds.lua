42cbdc02e782abf30b45666c90fc8351d0d3cb8e
yeonhwan <yeonhwan619@gmail.com>
2025-01-30 16:53:32 +0900

    Add modes nvim

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -9,12 +9,12 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("TS_add_missing_imports", { clear = true }),
-  desc = "TS_add_missing_imports",
-  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
-  callback = function()
-    vim.cmd([[TSToolsAddMissingImports]])
-    vim.cmd("write")
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   group = vim.api.nvim_create_augroup("TS_add_missing_imports", { clear = true }),
+--   desc = "TS_add_missing_imports",
+--   pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+--   callback = function()
+--     vim.cmd([[TSToolsAddMissingImports]])
+--     vim.cmd("write")
+--   end,
+-- })
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -8,4 +8,19 @@ return {
       },
     },
   },
+  -- modes nvim
+  {
+    "mvllow/modes.nvim",
+    config = function()
+      require("modes").setup({
+        colors = {
+          insert = "#2afc80",
+          visual = "#e0b4ff",
+          delete = "#e94127",
+          copy = "#e7f16e",
+        },
+        line_opacity = 0.35,
+      })
+    end,
+  },
 }
