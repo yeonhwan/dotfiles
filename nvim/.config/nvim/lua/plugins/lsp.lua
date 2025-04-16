00dd92390b2ecd7376f3aba41faabe91bbcb85ad
yeonhwan <yeonhwan619@gmail.com>
2025-04-16 19:20:33 +0900

    Add typescriptreact into lsplua

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -24,6 +24,7 @@ return {
     "L3MON4D3/LuaSnip",
     config = function()
       require("luasnip").filetype_extend("typescript", { "javascript" })
+      require("luasnip").filetype_extend("typescriptreact", { "javascript" })
       require("luasnip.loaders.from_vscode").lazy_load()
     end,
   },
